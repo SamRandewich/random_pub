84590ac864e4e1e502595851f08c8a5ee8ab62c4
SamRandewich <92059051+SamRandewich@users.noreply.github.com>
2021-10-06 21:40:43 +0100

    Add files via upload

---
+++ pubs.py
@@ -0,0 +1,22 @@
+
+import csv
+import random
+
+pubs_list = []
+i=1
+
+with open("open_pubs.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    line_count=0
+
+    for row in csv_reader:
+        pubs_list.append(row[1])
+        i+=1
+
+x = len(pubs_list)
+y=random.randint(0,x)
+print (pubs_list[y])
+
+    
+
+
